ff0095f3e26b29829dbd0555743b56fdb3394d92
korjun1993 <>
2023-02-27 21:10:48 +0900

    @Test 어노테이션 뒤에 개행 추가

--- app/src/test/java/com/example/AppTest.java
+++ app/src/test/java/com/example/AppTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
+    @Test
+    public void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
