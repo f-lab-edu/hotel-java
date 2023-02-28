8d6aff7816e7a42b81a6a74a5068174586cff0c2
korjun1993 <>
2023-03-01 00:40:54 +0900

    SpringBootApplication 어노테이션 추가

--- app/src/main/java/com/example/HotelJavaApplication.java
+++ app/src/main/java/com/example/HotelJavaApplication.java
@@ -1,11 +1,11 @@
 package com.example;
 
-public class HotelJavaApplication {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class HotelJavaApplication {
     public static void main(String[] args) {
-        System.out.println(new HotelJavaApplication().getGreeting());
+        SpringApplication.run(HotelJavaApplication.class, args);
     }
 }
--- app/src/test/java/com/example/HotelJavaApplicationTest.java
+++ app/src/test/java/com/example/HotelJavaApplicationTest.java
@@ -1,13 +1,4 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 public class HotelJavaApplicationTest {
-    @Test
-    public void appHasAGreeting() {
-        HotelJavaApplication classUnderTest = new HotelJavaApplication();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
 }
