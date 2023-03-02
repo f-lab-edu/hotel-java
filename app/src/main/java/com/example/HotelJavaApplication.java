09418a2ae1f930b775e9fef4a7066d70c1ff1acb
김호준 <45716246+korjun1993@users.noreply.github.com>
2023-03-02 12:08:44 +0900

    프로젝트 생성 (#1)
    * init hotel-java
    
    * Multi-module 프로젝트로 구성 변경
    
    * No newline at end of file 해결
    
    * @Test 어노테이션 뒤에 개행 추가
    
    * 스프링 환경에서 사용할 공통 플러그인 이름 변경
    
    * settings.gradle 파일 No newline at end of file 해결
    
    * 클래스 이름 변경 (App → HotelJavaApplication)
    
    * SpringBootApplication 어노테이션 추가
    
    ---------
    
    Co-authored-by: korjun1993 <>

---
+++ app/src/main/java/com/example/HotelJavaApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelJavaApplication.class, args);
+    }
+}
