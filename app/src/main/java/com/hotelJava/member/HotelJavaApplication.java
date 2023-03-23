6db3b7c7ae0875971357d25151097ba11c92476a
김호준 <corojoon93@naver.com>
2023-03-24 01:30:54 +0900

    feat: 패키지 이동 및 불필요한 코드 삭제

--- app/src/main/java/com/hotelJava/member/HotelJavaApplication.java
+++ app/src/main/java/com/hotelJava/member/HotelJavaApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.hotelJava.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app/src/test/java/com/hotelJava/HotelJavaApplicationTest.java
+++ app/src/test/java/com/hotelJava/HotelJavaApplicationTest.java
@@ -1,3 +1,3 @@
-package com.example;
+package com.hotelJava;
 
 public class HotelJavaApplicationTest {}
