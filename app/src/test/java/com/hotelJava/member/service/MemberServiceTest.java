c13b38ea0423597a9ead5096bf69fc245ce08d33
김호준 <corojoon93@naver.com>
2023-03-27 23:58:13 +0900

    feat: MemberService 테스트코드 수정
    - 문제 현상: Detached entity passed to persist 에러 발생
    - 원인1: `Member.id`= @GeneratedValue 로 설정했음에도  테스트용 Member 객체 `TEST_MEMBER`의 id를 1L로 설정함.
    - 원인2: 회원가입 테스트에서 Member 객체 `TEST_MEMBER`가 영속컨텍스트에 추가됌. 해당 테스트 메서드 종료 후 준영속 상태로 변경. 회원중복 테스트에서 동일한 객체를 persist 하려고 하여 에러 발생한 것으로 추정
    - 해결: 테스트 메서드마다 다른 객체를 사용할 수 있도록 수정

--- app/src/test/java/com/hotelJava/member/service/MemberServiceTest.java
+++ app/src/test/java/com/hotelJava/member/service/MemberServiceTest.java
@@ -5,55 +5,58 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.hotelJava.member.domain.Grade;
 import com.hotelJava.member.domain.Member;
 import com.hotelJava.member.domain.Role;
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @SpringBootTest
+@Transactional
 class MemberServiceTest {
 
   @Autowired private MemberService memberService;
 
   @Test
   @DisplayName("회원가입 기능을 테스트한다.")
-  @Transactional
   void signUp() {
+    // given
+    Member member = testMember();
+
     // when
-    memberService.signUp(TEST_MEMBER);
-    Member findMember = memberService.find(TEST_MEMBER);
+    memberService.signUp(member);
+    Member findMember = memberService.find(member);
 
     // then
-    assertThat(TEST_MEMBER).isEqualTo(findMember);
+    assertThat(member).isEqualTo(findMember);
   }
 
   @Test
   @DisplayName("이메일 중복 검사 기능을 테스트한다.")
-  @Transactional
   void isDuplicated() {
     // given
-    memberService.signUp(TEST_MEMBER);
+    Member member = testMember();
+    memberService.signUp(member);
 
     // when
-    boolean result = memberService.isDuplicatedEmail(TEST_MEMBER.getEmail());
+    boolean result = memberService.isDuplicatedEmail(member.getEmail());
 
     // then
     Assertions.assertThat(result).isTrue();
   }
 
   /** test fixture */
-  static final Member TEST_MEMBER =
-      Member.builder()
-          .id(1L)
-          .email("testcode@example.com")
-          .name("testcode")
-          .phone("010-1111-2222")
-          .password("abcd1234")
-          .grade(Grade.NORMAL)
-          .role(Role.USER)
-          .build();
+  static Member testMember() {
+    return Member.builder()
+        .email("testcode@example.com")
+        .name("testcode")
+        .phone("010-1111-2222")
+        .password("abcd1234")
+        .grade(Grade.NORMAL)
+        .role(Role.USER)
+        .build();
+  }
 }
