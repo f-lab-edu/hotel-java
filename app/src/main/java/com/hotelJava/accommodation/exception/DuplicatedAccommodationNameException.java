8c1bd0dc32afbb6e546ad93add9fb4ecceef7843
ktpark <>
2023-04-03 17:02:53 +0900

    feat: 불필요한 코드 삭제

--- app/src/main/java/com/hotelJava/accommodation/exception/DuplicatedAccommodationNameException.java
+++ app/src/main/java/com/hotelJava/accommodation/exception/DuplicatedAccommodationNameException.java
@@ -9,16 +9,4 @@ public class DuplicatedAccommodationNameException extends RuntimeException {
     public DuplicatedAccommodationNameException(String message) {
         super(message);
     }
-
-    public DuplicatedAccommodationNameException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DuplicatedAccommodationNameException(Throwable cause) {
-        super(cause);
-    }
-
-    public DuplicatedAccommodationNameException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
