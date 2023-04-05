25f90f9a72410119c99c8811cc2fdc5468f8e5d6
김호준 <corojoon93@naver.com>
2023-04-06 00:48:43 +0900

    feat: 회원가입시 비밀번호 암호화

--- app/src/main/java/com/hotelJava/member/service/MemberService.java
+++ app/src/main/java/com/hotelJava/member/service/MemberService.java
@@ -3,24 +3,30 @@ package com.hotelJava.member.service;
 import static com.hotelJava.member.util.MemberMapper.MEMBER_MAPPER;
 
 import com.hotelJava.common.error.ErrorCode;
-import com.hotelJava.common.error.exception.BadRequestException;import com.hotelJava.member.domain.Member;
+import com.hotelJava.common.error.exception.BadRequestException;
+import com.hotelJava.member.domain.Member;
 import com.hotelJava.member.domain.ProfileInfo;
 import com.hotelJava.member.dto.SignUpRequestDto;
 import com.hotelJava.member.repository.MemberRepository;
+import com.hotelJava.member.util.MemberPasswordEncoder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
   private final MemberRepository memberRepository;
+  private final MemberPasswordEncoder passwordEncoder;
 
   public void signUp(ProfileInfo profileInfo) {
     if (isDuplicatedEmail(profileInfo.getEmail())) {
       throw new BadRequestException(ErrorCode.DUPLICATED_EMAIL_FOUND);
     }
-    Member member = MEMBER_MAPPER.toEntity((SignUpRequestDto) profileInfo);
+    SignUpRequestDto encrypted = passwordEncoder.encrypt((SignUpRequestDto) profileInfo);
+    Member member = MEMBER_MAPPER.toEntity(encrypted);
     memberRepository.save(member);
   }
 
