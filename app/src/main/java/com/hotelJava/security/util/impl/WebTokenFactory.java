33288b1f760c5218d7f5124197b89889f68b3f71
김호준 <corojoon93@naver.com>
2023-04-09 19:04:44 +0900

    chore: 불필요 파일 제거

--- app/src/main/java/com/hotelJava/security/util/impl/WebTokenFactory.java
+++
@@ -1,7 +0,0 @@
-package com.hotelJava.security.util.impl;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface WebTokenFactory {
-  String generate(UserDetails userDetails);
-}
