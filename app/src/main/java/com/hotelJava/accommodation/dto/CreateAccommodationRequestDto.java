45ade28090c72f55eebdb1a007f1196ad8162fe2
ktpark <>
2023-04-11 12:20:33 +0900

    feat: 숙소 등록 시 요청 dto 설계

--- app/src/main/java/com/hotelJava/accommodation/dto/CreateAccommodationRequestDto.java
+++ app/src/main/java/com/hotelJava/accommodation/dto/CreateAccommodationRequestDto.java
@@ -1,13 +1,11 @@
 package com.hotelJava.accommodation.dto;
 
 import com.hotelJava.accommodation.domain.AccommodationType;
+import com.hotelJava.accommodation.picture.dto.PictureResponseDto;
 import com.hotelJava.common.embeddable.Address;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
-import java.time.LocalDate;
+import jakarta.validation.constraints.Pattern;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,20 +17,22 @@ import org.hibernate.validator.constraints.Length;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class AccommodationRequestDto {
+public class CreateAccommodationRequestDto {
 
   @NotBlank(message = "숙소 이름을 입력해주세요.")
   @Length(max = 20, message = "숙소 이름은 20자 이내로 입력해주세요.")
   private String name;
 
-  @Enumerated(EnumType.STRING)
+  @NotBlank(message = "숙소 번호의 형식이 맞지 않습니다.")
+  @Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}")
+  private String phoneNumber;
+  
+  @NotBlank(message = "숙소 타입을 선택해주세요.")
   private AccommodationType accommodationType;
 
-  @Embedded private Address address;
+  private Address address;
 
-  @Future private LocalDate checkInDate;
-
-  @Future private LocalDate checkOutDate;
-
-  private int guestCount;
+  private List<PictureResponseDto> pictureResponseDtos;
+  
+  private String description;
 }
