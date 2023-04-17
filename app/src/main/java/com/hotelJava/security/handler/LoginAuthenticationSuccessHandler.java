19d0375b5d7811cd8cb662c0e9f2d3c4b999a866
김호준 <corojoon93@naver.com>
2023-04-17 15:55:24 +0900

    feat: jwt token 기반 로그인 기능

--- app/src/main/java/com/hotelJava/security/handler/LoginAuthenticationSuccessHandler.java
+++ app/src/main/java/com/hotelJava/security/handler/LoginAuthenticationSuccessHandler.java
@@ -1,42 +1,30 @@
 package com.hotelJava.security.handler;
 
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hotelJava.security.config.JwtConfig;
-import com.hotelJava.security.token.PostAuthenticationToken;
-import com.hotelJava.security.util.specification.JwtPayload;
-import com.hotelJava.security.util.specification.JwtTokenFactory;
+import com.hotelJava.security.token.LoginPostAuthenticationToken;
+import com.hotelJava.security.util.impl.JwtTokenFactory;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LoginAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
   private final JwtTokenFactory jwtTokenFactory;
-  private final ObjectMapper objectMapper;
 
   @Override
   public void onAuthenticationSuccess(
-      HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-      throws IOException {
+      HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    LoginPostAuthenticationToken postAuthToken = (LoginPostAuthenticationToken) authentication;
 
-    PostAuthenticationToken postAuthToken = (PostAuthenticationToken) authentication;
+    String token =
+        jwtTokenFactory.generate(postAuthToken.getJwtPayload(), System.currentTimeMillis());
 
-    Date expireTime = new Date(System.currentTimeMillis() + JwtConfig.EXPIRE_TIME);
-    JwtPayload payload = postAuthToken.getJwtPayload(expireTime);
-    Algorithm algorithm = Algorithm.HMAC512(JwtConfig.SECRET);
-
-    String token = jwtTokenFactory.generate(payload, algorithm);
-
-    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-    response.setHeader("Authorization", objectMapper.writeValueAsString(token));
+    response.setHeader("Authorization", token);
   }
 }
---
+++ app/src/main/java/com/hotelJava/security/util/specification/JwtTokenDecoder.java
@@ -0,0 +1,46 @@
+package com.hotelJava.security.util.specification;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.hotelJava.common.error.ErrorCode;
+import com.hotelJava.common.error.exception.BadRequestException;
+import com.hotelJava.security.MemberDetails;
+import com.hotelJava.security.config.JwtConfigurationProperties;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtTokenDecoder {
+
+  private final JwtConfigurationProperties jwtConfig;
+
+  public MemberDetails decode(String jwtToken) {
+    DecodedJWT decodedJWT =
+        verify(jwtToken).orElseThrow(() -> new BadRequestException(ErrorCode.AUTHENTICATION_FAIL));
+    String email = decodedJWT.getSubject();
+    List<? extends GrantedAuthority> authorities =
+        decodedJWT.getClaim("roles").asList(SimpleGrantedAuthority.class);
+    return new MemberDetails(email, "none", authorities);
+  }
+
+  private Optional<DecodedJWT> verify(String jwtToken) {
+    DecodedJWT decodedJWT = null;
+    try {
+      JWTVerifier verifier = JWT.require(Algorithm.HMAC512(jwtConfig.secret())).build();
+      decodedJWT = verifier.verify(jwtToken);
+    } catch (JWTVerificationException e) {
+      log.error("not verified JWT token");
+    }
+    return Optional.ofNullable(decodedJWT);
+  }
+}
