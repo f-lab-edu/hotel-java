1e71722df157a8cdde120580b4f97fb20c2559fe
김호준 <45716246+korjun1993@users.noreply.github.com>
2023-04-17 14:08:24 +0900

    chore: 리베이스 과정에서 누락된 파일 추가 (#34)

---
+++ app/src/main/java/com/hotelJava/security/filter/LoginAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.hotelJava.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hotelJava.security.dto.LoginDto;
+import com.hotelJava.security.token.LoginPreAuthenticationToken;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+@Slf4j
+public class LoginAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+  @Autowired private ObjectMapper objectMapper;
+
+  public LoginAuthenticationFilter(String defaultUrl) {
+    super(defaultUrl);
+  }
+
+  @Override
+  public Authentication attemptAuthentication(
+      HttpServletRequest request, HttpServletResponse response)
+      throws AuthenticationException, IOException {
+
+    LoginDto loginDto = getLoginDto(request);
+    LoginPreAuthenticationToken preAuthToken = new LoginPreAuthenticationToken(loginDto);
+
+    return getAuthenticationManager().authenticate(preAuthToken);
+  }
+
+  private LoginDto getLoginDto(HttpServletRequest request) throws IOException {
+    return objectMapper.readValue(request.getInputStream(), LoginDto.class);
+  }
+}
---
+++ app/src/main/java/com/hotelJava/security/util/impl/JwtTokenFactory.java
@@ -0,0 +1,26 @@
+package com.hotelJava.security.util.impl;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.hotelJava.security.config.JwtConfigurationProperties;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtTokenFactory {
+
+  private final JwtConfigurationProperties jwtConfig;
+
+  public String generate(JwtPayload payload, long currentTime) {
+    return JWT.create()
+        .withSubject(payload.getSubject())
+        .withClaim("roles", payload.getRoles())
+        .withExpiresAt(new Date(currentTime + jwtConfig.validityTime().toMillis()))
+        .withIssuer(jwtConfig.issuer())
+        .sign(Algorithm.HMAC512(jwtConfig.secret()));
+  }
+}
