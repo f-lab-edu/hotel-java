d0b89863eccf3f227ad5a2ed35f2d8f8f01040c7
김호준 <corojoon93@naver.com>
2023-04-18 23:16:02 +0900

    feat: MemberService 회원가입 테스트코드 수정

--- app/src/test/java/com/hotelJava/member/service/MemberServiceTest.java
+++ app/src/test/java/com/hotelJava/member/service/MemberServiceTest.java
@@ -1,15 +1,14 @@
 package com.hotelJava.member.service;
 
-import static com.hotelJava.member.MemberTestFixture.getTestMember;
-import static com.hotelJava.member.MemberTestFixture.getTestSignUpDto;
-import static com.hotelJava.member.util.MemberMapper.MEMBER_MAPPER;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.hotelJava.member.MemberTestFixture.getMember;
+import static com.hotelJava.member.MemberTestFixture.getSignUpDto;
 
 import com.hotelJava.member.domain.Member;
 import com.hotelJava.member.dto.SignUpRequestDto;
 import com.hotelJava.member.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,21 +27,26 @@ class MemberServiceTest {
   @DisplayName("회원가입 기능을 테스트한다.")
   void signUp() {
     // given
-    SignUpRequestDto signUpDto = getTestSignUpDto();
+    SignUpRequestDto signUpDto = getSignUpDto();
 
     // when
     memberService.signUp(signUpDto);
     Member findMember = memberRepository.findByEmail(signUpDto.getEmail()).orElse(null);
 
     // then
-    assertThat(MEMBER_MAPPER.toSignUpRequestDto(findMember)).isEqualTo(signUpDto);
+    SoftAssertions.assertSoftly(
+        softly -> {
+          softly.assertThat(findMember.getEmail()).isEqualTo(signUpDto.getEmail());
+          softly.assertThat(findMember.getName()).isEqualTo(signUpDto.getName());
+          softly.assertThat(findMember.getPhone()).isEqualTo(signUpDto.getPhone());
+        });
   }
 
   @Test
   @DisplayName("이메일 중복 검사 기능을 테스트한다.")
   void isDuplicated() {
     // given
-    Member member = getTestMember();
+    Member member = getMember();
     memberRepository.save(member);
 
     // when
