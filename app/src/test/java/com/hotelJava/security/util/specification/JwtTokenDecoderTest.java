bd97e20ed744389d0244013b5ff85c9f252547d8
김호준 <corojoon93@naver.com>
2023-04-20 13:24:33 +0900

    feat: Security 관련 테스트코드 작성

---
+++ app/src/test/java/com/hotelJava/security/util/specification/JwtTokenDecoderTest.java
@@ -0,0 +1,57 @@
+package com.hotelJava.security.util.specification;
+
+import com.hotelJava.common.error.exception.BadRequestException;
+import com.hotelJava.member.MemberTestFixture;
+import com.hotelJava.member.domain.Member;
+import com.hotelJava.security.MemberDetails;
+import com.hotelJava.security.util.impl.JwtPayload;
+import com.hotelJava.security.util.impl.JwtTokenDecoder;
+import com.hotelJava.security.util.impl.JwtTokenFactory;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenDecoderTest {
+
+  @Autowired private JwtTokenDecoder jwtTokenDecoder;
+  @Autowired private JwtTokenFactory jwtTokenFactory;
+
+  @Test
+  @DisplayName("유효한 토큰은 디코딩했을 때 MemberDetails 정보를 가져올 수 있다")
+  void verify_DecodedJWT() {
+    // given
+    Member member = MemberTestFixture.getMember();
+    String token = getJwtToken(member, System.currentTimeMillis());
+
+    // when
+    MemberDetails memberDetails = jwtTokenDecoder.decode(token);
+
+    // then
+    Assertions.assertThat(new MemberDetails(member)).isEqualTo(memberDetails);
+  }
+
+  @Test
+  @DisplayName("유효 시간이 지난 토큰은 디코딩했을 때 예외가 발생한다")
+  void verify_Exception() {
+    // given
+    Member member = MemberTestFixture.getMember();
+    String token = getJwtToken(member, 1000);
+
+    // when, then
+    Assertions.assertThatThrownBy(() -> jwtTokenDecoder.decode(token))
+        .isInstanceOf(BadRequestException.class);
+  }
+
+  private String getJwtToken(Member member, long currentTime) {
+    JwtPayload payload =
+        JwtPayload.builder()
+            .subject(member.getEmail())
+            .roles(List.of(member.getRole().name()))
+            .build();
+    return jwtTokenFactory.generate(payload, currentTime);
+  }
+}
