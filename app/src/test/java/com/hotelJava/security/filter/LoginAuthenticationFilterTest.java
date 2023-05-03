739c9a19963fdaabd4c724989921f4f74a99cb79
김호준 <corojoon93@naver.com>
2023-05-03 17:38:30 +0900

    feat: LoginAuthenticationFilter 테스트시, 외부  협력체 Mocking

--- app/src/test/java/com/hotelJava/security/filter/LoginAuthenticationFilterTest.java
+++ app/src/test/java/com/hotelJava/security/filter/LoginAuthenticationFilterTest.java
@@ -1,64 +1,61 @@
 package com.hotelJava.security.filter;
 
+import static org.mockito.Mockito.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import com.hotelJava.common.error.exception.BadRequestException;
-import com.hotelJava.member.MemberTestFixture;
-import com.hotelJava.member.dto.SignUpRequestDto;
-import com.hotelJava.member.service.MemberService;
 import com.hotelJava.security.dto.LoginDto;
-import com.hotelJava.security.token.LoginPostAuthenticationToken;
+import com.hotelJava.security.token.LoginPreAuthenticationToken;
 import java.io.IOException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootTest
 class LoginAuthenticationFilterTest {
 
+  private final Faker faker = Faker.instance();
   private final ObjectMapper objectMapper = new ObjectMapper();
-  @Autowired private LoginAuthenticationFilter loginFilter;
-  @Autowired private MemberService memberService;
+  private final LoginAuthenticationFilter loginFilter = new LoginAuthenticationFilter("/login");
+  @Spy private AuthenticationManager authenticationManager;
+
+  @BeforeEach
+  void setUp() {
+    loginFilter.setAuthenticationManager(authenticationManager);
+  }
 
   @Test
-  @DisplayName("인증에 성공한다면 Authentication 객체가 리턴된다")
+  @DisplayName("HttpRequestBody 안에 LoginDto 데이터가 있다면, AuthenticationManager.authenticate 메서드가 호출된다")
   void attemptAuthentication_Authentication_success() throws IOException {
     // given
     MockHttpServletRequest request = new MockHttpServletRequest();
     MockHttpServletResponse response = new MockHttpServletResponse();
-
-    SignUpRequestDto signUpDto = MemberTestFixture.getSignUpDto();
-    memberService.signUp(signUpDto);
+    String email = faker.internet().emailAddress();
+    String password = faker.internet().password();
+    request.setContent(objectMapper.writeValueAsBytes(new LoginDto(email, password)));
 
     // when
-    LoginDto dto = new LoginDto(signUpDto.getEmail(), signUpDto.getPassword());
-    request.setContent(objectMapper.writeValueAsBytes(dto));
-    Authentication authentication = loginFilter.attemptAuthentication(request, response);
+    loginFilter.attemptAuthentication(request, response);
 
     // then
-    Assertions.assertThat(authentication).isInstanceOf(LoginPostAuthenticationToken.class);
+    verify(authenticationManager).authenticate(any(LoginPreAuthenticationToken.class));
   }
 
   @Test
-  @DisplayName("인증에 실패한다면 BadRequestException 예외가 발생한다")
-  void attemptAuthentication_Exception_failed() throws IOException {
+  @DisplayName("Http 요청의 바디에 LoginDto 데이터가 없다면, BadRequestException 예외가 발생한다")
+  void attemptAuthentication_Exception_failed() {
     // given
     MockHttpServletRequest request = new MockHttpServletRequest();
     MockHttpServletResponse response = new MockHttpServletResponse();
 
-    String email = Faker.instance().internet().emailAddress();
-    String password = Faker.instance().internet().password();
-
-    // when
-    LoginDto dto = new LoginDto(email, password);
-    request.setContent(objectMapper.writeValueAsBytes(dto));
-
-    // then
+    // when, then
     Assertions.assertThatThrownBy(() -> loginFilter.attemptAuthentication(request, response))
         .isInstanceOf(BadRequestException.class);
   }
