23187adba8797d2fb57069588f3fbb2c03a5bf49
김호준 <corojoon93@naver.com>
2023-05-07 01:22:13 +0900

    feat: 예약확정 기능 추가 - 예약에 대해 결제 승인 여부를 확인후, 정상 결제 상황이라면 재고를 감소시키고 예약상태를 변경시킨다.

--- app/src/main/java/com/hotelJava/reservation/domain/Reservation.java
+++ app/src/main/java/com/hotelJava/reservation/domain/Reservation.java
@@ -1,58 +1,46 @@
 package com.hotelJava.reservation.domain;
 
 import com.hotelJava.common.embeddable.CheckDate;
+import com.hotelJava.common.error.ErrorCode;
+import com.hotelJava.common.error.exception.BadRequestException;
 import com.hotelJava.common.util.BaseTimeEntity;
 import com.hotelJava.member.domain.Member;
-import com.hotelJava.payment.domain.PaymentType;
+import com.hotelJava.payment.domain.Payment;
 import com.hotelJava.room.domain.Room;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.NoArgsConstructor;import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Entity
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Reservation extends BaseTimeEntity {
+public class Reservation extends BaseTimeEntity implements GuestInfo {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String name;
-
   private String reservationNo;
 
-  private String accommodationName;
-
-  private String roomName;
+  @Builder.Default
+  @Enumerated(EnumType.STRING)
+  private ReservationStatus status = ReservationStatus.PAYMENT_PENDING;
 
   @Embedded private CheckDate checkDate;
 
   private int numberOfGuests;
 
-  @Enumerated(EnumType.STRING)
-  private PaymentType paymentType;
-
-  private String phoneNumber;
-
-  @Enumerated(EnumType.STRING)
-  private ReservationStatus status;
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "payment_id")
+  private Payment payment;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id")
@@ -61,4 +49,32 @@ public class Reservation extends BaseTimeEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "room_id")
   private Room room;
+
+  public Reservation(GuestInfo info) {
+    this.member = info.getMember();
+    this.checkDate = info.getCheckDate();
+    this.numberOfGuests = info.getNumberOfGuests();
+    this.payment = new Payment(room.calcPrice());
+  }
+
+  public Reservation confirm() {
+    validateReservation();
+    if (!payment.isPaymentSuccess()) {
+      throw new BadRequestException(ErrorCode.PAYMENT_FAIL);
+    }
+    room.reduceStock(checkDate);
+    // TODO: 결제승인
+    status = ReservationStatus.RESERVATION_COMPLETED;
+    // payment.approve();
+    return this;
+  }
+
+  public void validateReservation() {
+    if (!room.isAvailableReservation(numberOfGuests, checkDate)) {
+      // TODO: 결제승인거절
+      // payment.decline();
+      log.info("payment declined");
+      throw new BadRequestException(ErrorCode.PAYMENT_FAIL);
+    }
+  }
 }
