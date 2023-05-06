17a230cedcdf69775cc62b95d814447298ec099c
김호준 <corojoon93@naver.com>
2023-05-07 01:00:18 +0900

    hotfix: RoomAvailability 삭제

--- app/src/test/java/com/hotelJava/accommodation/application/port/BeforeEachInit.java
+++ app/src/test/java/com/hotelJava/accommodation/application/port/BeforeEachInit.java
@@ -1,23 +1,25 @@
 package com.hotelJava.accommodation.application.port;
 
-import com.hotelJava.accommodation.adapter.persistence.AccommodationRepository;import com.hotelJava.accommodation.domain.Accommodation;
+import com.hotelJava.accommodation.adapter.persistence.AccommodationRepository;
+import com.hotelJava.accommodation.domain.Accommodation;
 import com.hotelJava.accommodation.domain.AccommodationType;
 import com.hotelJava.common.embeddable.Address;
 import com.hotelJava.common.embeddable.CheckDate;
 import com.hotelJava.common.embeddable.CheckTime;
-import com.hotelJava.payment.domain.PaymentType;
 import com.hotelJava.picture.domain.Picture;
 import com.hotelJava.picture.domain.PictureInfo;
 import com.hotelJava.picture.domain.PictureType;
 import com.hotelJava.reservation.domain.Reservation;
 import com.hotelJava.reservation.domain.ReservationStatus;
 import com.hotelJava.room.domain.Room;
-import com.hotelJava.room.domain.RoomAvailability;
-import org.junit.jupiter.api.BeforeEach;import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class BeforeEachInit {
 
   @Autowired AccommodationRepository accommodationRepository;
@@ -62,49 +64,24 @@ public class BeforeEachInit {
             .build();
 
     // 룸
-    Room room1 =
-        Room.builder().name("1번 룸").price(1000).maxOccupancy(2).checkTime(checkTime).build();
-    Room room2 =
-        Room.builder().name("2번 룸").price(2000).maxOccupancy(2).checkTime(checkTime).build();
-    Room room3 =
-        Room.builder().name("3번 룸룸룸").price(30000).maxOccupancy(2).checkTime(checkTime).build();
+    Room room1 = Room.builder().name("1번 룸").price(1000).maxOccupancy(2).build();
+    Room room2 = Room.builder().name("2번 룸").price(2000).maxOccupancy(2).build();
+    Room room3 = Room.builder().name("3번 룸룸룸").price(30000).maxOccupancy(2).build();
 
     // 예약
     Reservation reservation1 =
         Reservation.builder()
-            .name("예약자 성명1")
             .reservationNo("000000002023")
-            .accommodationName("예약 숙소 명")
-            .roomName("예약 룸 명")
             .checkDate(checkDate)
             .numberOfGuests(2)
-            .paymentType(PaymentType.CARD)
-            .phoneNumber("010-1234-5678")
             .status(ReservationStatus.RESERVATION_CANCEL)
             .build();
     Reservation reservation2 =
         Reservation.builder()
-            .name("예약자 성명2")
             .reservationNo("000000002023")
-            .accommodationName("예약 숙소 명")
-            .roomName("예약 룸 명")
             .checkDate(checkDate)
             .numberOfGuests(2)
-            .paymentType(PaymentType.CARD)
-            .phoneNumber("010-1234-5678")
             .status(ReservationStatus.RESERVATION_CANCEL)
-            .build();
-
-    // 룸 날짜별 예약 상태
-    RoomAvailability roomAvailability1 =
-        RoomAvailability.builder()
-            .reservationDate(LocalDate.of(2023, 5, 1))
-            .status(ReservationStatus.RESERVATION_COMPLETED)
-            .build();
-    RoomAvailability roomAvailability2 =
-        RoomAvailability.builder()
-            .reservationDate(LocalDate.of(2023, 5, 2))
-            .status(ReservationStatus.RESERVATION_COMPLETED)
             .build();
 
     // 지역 정보
@@ -137,14 +114,10 @@ public class BeforeEachInit {
             .status(ReservationStatus.RESERVATION_AVAILABLE)
             .build();
 
-    room1.addRoomAvailability(roomAvailability1);
-    room1.addRoomAvailability(roomAvailability2);
     room1.addReservation(reservation1);
     room1.addPicture(roomPicture1);
     room1.addPicture(roomPicture2);
 
-    room3.addRoomAvailability(roomAvailability1);
-    room3.addRoomAvailability(roomAvailability2);
     room3.addReservation(reservation2);
 
     accommodation1.createAccommodation(List.of(room1, room2), accommodationPicture);
