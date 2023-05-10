348c56c894462a57925dcea40ed2433e28247530
김호준 <corojoon93@naver.com>
2023-05-10 23:33:57 +0900

    fix: GuestInfo 도메인 필드 제거

--- app/src/main/java/com/hotelJava/reservation/domain/GuestInfo.java
+++ app/src/main/java/com/hotelJava/reservation/domain/GuestInfo.java
@@ -1,10 +1,12 @@
 package com.hotelJava.reservation.domain;
 
 import com.hotelJava.common.embeddable.CheckDate;
-import com.hotelJava.member.domain.Member;
 
 public interface GuestInfo {
-  Member getMember();
+
+  String getName();
+
+  String getPhone();
 
   int getNumberOfGuests();
 
