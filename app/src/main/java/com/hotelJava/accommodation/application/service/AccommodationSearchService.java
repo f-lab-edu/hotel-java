ab2a7252dc1a4dc70482f62b40e84b007d658c16
박경탁 <68748397+DevKTak@users.noreply.github.com>
2023-05-12 06:31:58 +0900

    feat: Role(권한)에 따른 숙소 조회 분기 처리 (#52)

--- app/src/main/java/com/hotelJava/accommodation/application/service/AccommodationSearchService.java
+++ app/src/main/java/com/hotelJava/accommodation/application/service/AccommodationSearchService.java
@@ -8,6 +8,8 @@ import com.hotelJava.accommodation.dto.FindAccommodationResponseDto;
 import com.hotelJava.accommodation.util.AccommodationMapper;
 import com.hotelJava.common.error.ErrorCode;
 import com.hotelJava.common.error.exception.InternalServerException;
+import com.hotelJava.member.domain.Role;
+import com.hotelJava.reservation.domain.ReservationStatus;
 import com.hotelJava.room.domain.Room;
 import java.time.LocalDate;
 import java.util.List;
@@ -21,33 +23,48 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AccommodationSearchService implements FindAccommodationQuery {
 
-    private final AccommodationRepository accommodationRepository;
+  private final AccommodationRepository accommodationRepository;
 
-    private final AccommodationMapper accommodationMapper;
+  private final AccommodationMapper accommodationMapper;
 
-    public List<FindAccommodationResponseDto> findAccommodations(
-            AccommodationType type,
-            String firstLocation,
-            String secondLocation,
-            String name,
-            LocalDate checkInDate,
-            LocalDate checkOutDate,
-            int guestCount) {
-        List<Accommodation> accommodations =
-                accommodationRepository.findAccommodations(
-                        type, firstLocation, secondLocation, name, checkInDate, checkOutDate, guestCount);
-        return accommodations.stream()
-                .map(
-                        accommodation -> {
-                            int minimumRoomPrice =
-                                    accommodation.getRooms().stream()
-                                            .mapToInt(Room::getPrice)
-                                            .min()
-                                            .orElseThrow(
-                                                    () -> new InternalServerException(ErrorCode.NO_MINIMUM_PRICE_FOUND));
-                            return accommodationMapper.toFindAccommodationResponseDto(
-                                    minimumRoomPrice, accommodation);
-                        })
-                .collect(Collectors.toList());
+  public List<FindAccommodationResponseDto> findAccommodations(
+      AccommodationType type,
+      String firstLocation,
+      String secondLocation,
+      String name,
+      LocalDate checkInDate,
+      LocalDate checkOutDate,
+      int guestCount,
+      Role role) {
+    ReservationStatus reservationStatus;
+
+    if (role.equals(Role.ADMIN)) {
+      reservationStatus = ReservationStatus.RESERVATION_COMPLETED;
+    } else {
+      reservationStatus = ReservationStatus.RESERVATION_AVAILABLE;
     }
+    List<Accommodation> accommodations =
+        accommodationRepository.findAccommodations(
+            type,
+            firstLocation,
+            secondLocation,
+            name,
+            checkInDate,
+            checkOutDate,
+            guestCount,
+            reservationStatus);
+    return accommodations.stream()
+        .map(
+            accommodation -> {
+              int minimumRoomPrice =
+                  accommodation.getRooms().stream()
+                      .mapToInt(Room::getPrice)
+                      .min()
+                      .orElseThrow(
+                          () -> new InternalServerException(ErrorCode.NO_MINIMUM_PRICE_FOUND));
+              return accommodationMapper.toFindAccommodationResponseDto(
+                  minimumRoomPrice, accommodation);
+            })
+        .collect(Collectors.toList());
+  }
 }
