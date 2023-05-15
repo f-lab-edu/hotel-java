aadfe1ee2a4e8eb7714d0806faf2bd8ca49bc979
김호준 <corojoon93@naver.com>
2023-05-16 02:28:49 +0900

    feat: 커맨드 validation

--- app/src/main/java/com/hotelJava/member/application/port/in/command/ChangePasswordCommand.java
+++ app/src/main/java/com/hotelJava/member/application/port/in/command/ChangePasswordCommand.java
@@ -1,12 +1,17 @@
 package com.hotelJava.member.application.port.in.command;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ChangePasswordCommand {
   @NotBlank(message = "비밀번호를 입력하세요.")
-  private final String password;
+  private String password;
 }
--- app/src/main/java/com/hotelJava/member/application/port/in/command/ChangeProfileCommand.java
+++ app/src/main/java/com/hotelJava/member/application/port/in/command/ChangeProfileCommand.java
@@ -1,12 +1,21 @@
 package com.hotelJava.member.application.port.in.command;
 
 import com.hotelJava.member.domain.specification.Profile;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ChangeProfileCommand implements Profile {
+  @NotBlank(message = "이름을 입력하세요.")
   private String name;
+
+  @NotBlank(message = "휴대폰 번호를 입력하세요.")
   private String phone;
 }
